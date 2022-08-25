ae7acbc3df43a81eda7a7b887e1e5b60fa3f5190
ammolder <98980721+ammolder@users.noreply.github.com>
2022-08-25 12:18:45 +0200

    Update hw-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,4 +5,20 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const CURRENT_TIME = 'videoplayer-current-time';
-const 
+
+const onTimeUpdate = function (data) {
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+player.setCurrentTime(restoreTime());
+
+function restoreTime() {
+  const savedCurrentTime = localStorage.getItem(CURRENT_TIME);
+  if (savedCurrentTime) {
+    const parseTime = JSON.parse(savedCurrentTime);
+    return parseTime.seconds;
+  }
+  return 0;
+}
