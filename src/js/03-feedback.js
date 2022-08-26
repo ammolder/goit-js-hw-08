fbe1e63d53f9326e0592c858f42b1b704a7c52b2
ammolder <98980721+ammolder@users.noreply.github.com>
2022-08-26 12:03:49 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,23 +5,18 @@ const refs = {
   input: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
-populateTextarea();
 
 const FEEDBACK_KEY = 'feedback-form-state';
-const formData = {
-  email: '',
-  message: '',
-};
-console.log('formData :', formData);
-
+const formData = {};
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onLocalStorageSet, 500));
+
+populateTextarea();
 
 function onLocalStorageSet(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
-console.log('formData :', formData);
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -32,10 +27,8 @@ function onFormSubmit(e) {
 
 function populateTextarea() {
   const savedMessage = localStorage.getItem(FEEDBACK_KEY);
-  const parseMessage = JSON.parse(savedMessage);
-  console.log('parseMessage :', parseMessage);
-
-  if (savedMessage === true) {
+  if (savedMessage) {
+    const parseMessage = JSON.parse(savedMessage);
     refs.input.value = parseMessage.email;
     refs.textarea.value = parseMessage.message;
   }
