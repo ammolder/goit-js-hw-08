525fba7c26a03adcecfd0c3e9e5205733cea8220
ammolder <98980721+ammolder@users.noreply.github.com>
2022-08-26 11:55:02 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,7 +8,12 @@ const refs = {
 populateTextarea();
 
 const FEEDBACK_KEY = 'feedback-form-state';
-const formData = {};
+const formData = {
+  email: '',
+  message: '',
+};
+console.log('formData :', formData);
+
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onLocalStorageSet, 500));
 
@@ -16,6 +21,7 @@ function onLocalStorageSet(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
+console.log('formData :', formData);
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -26,8 +32,10 @@ function onFormSubmit(e) {
 
 function populateTextarea() {
   const savedMessage = localStorage.getItem(FEEDBACK_KEY);
-  if (savedMessage) {
-    const parseMessage = JSON.parse(savedMessage);
+  const parseMessage = JSON.parse(savedMessage);
+  console.log('parseMessage :', parseMessage);
+
+  if (savedMessage === true) {
     refs.input.value = parseMessage.email;
     refs.textarea.value = parseMessage.message;
   }
