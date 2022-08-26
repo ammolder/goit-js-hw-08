1babf84f5931b9219aba298e1f2a098257b82bd3
ammolder <98980721+ammolder@users.noreply.github.com>
2022-08-26 11:00:18 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,6 +5,7 @@ const refs = {
   input: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
+populateTextarea();
 
 const FEEDBACK_KEY = 'feedback-form-state';
 const formData = {};
@@ -13,15 +14,11 @@ refs.form.addEventListener('input', throttle(onLocalStorageSet, 500));
 
 function onLocalStorageSet(e) {
   formData[e.target.name] = e.target.value;
-  console.log('formData :', formData);
   localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
 
-populateTextarea();
-
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log(formData);
   e.currentTarget.reset();
   localStorage.removeItem(FEEDBACK_KEY);
 }
